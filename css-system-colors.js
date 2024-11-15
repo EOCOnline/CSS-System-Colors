94a20ec6da64df95866a5cfd2f1fe8d8f8d6788d
JCornelison <gitter@VashonDesign.com>
2024-11-14 17:50:43 -0800

    Working back to FileReader - if fetch fails...

--- css-system-colors.js
+++ css-system-colors.js
@@ -92,7 +92,7 @@ function resetWebPage() {
  */
 
 // Read JSON File with system colors
-async function readSystemColors() {
+async function fetchSystemColors() {
   try {
     // avoid CORS errors when reading a local file!
     if (logLevel > 1) console.log("Fetching JSON file:" + sourceJson);
@@ -104,7 +104,57 @@ async function readSystemColors() {
     processJson(json);
   } catch (error) {
     console.error("Fetch error: " + error.message);
+    console.error("Will use FileReader next, assuming html is a local file");
+    try {
+      const json = await readJSONFile(sourceJson);
+      processJson(json);
+    } catch (error) {
+      console.error("FileReader error: " + error.message);
+    }
   }
+}
+
+async function readSystemColors() {
+  let file = new File([""], sourceJson, { type: "application/json" });
+  readJSONFile(file).then(
+    json => {
+      console.log("%c" + "Read system colors file: " + file.name + " with " + file.size + " char", "color:maroon;font-weight:bold;");
+      if (!validateJson(json)) {
+        console.error("Invalid JSON: " + json);
+        alert("Invalid JSON: " + json);
+        return;
+      }
+      try {
+        generateSystemColors(json);
+      } catch (e) {
+        console.error("Error building list of System Colors: " + e.message);
+      }
+    }
+  ).catch(error => {
+    console.error("Error reading JSON file: " + error.message);
+    alert("Error reading JSON file (" + file.name + "): \n" + error.message);
+  });
+}
+
+// Use fileReader instead of fetch, to avoid CORS errors with local files
+async function readJSONFile(file) {
+  return new Promise((resolve, reject) => {
+    let fileReader = new FileReader();
+    fileReader.onload = event => {
+      try {
+        const jsonString = event.target.result;
+        const json = JSON.parse(jsonString);
+        resolve(json);
+      } catch (error) {
+        alert("Invalid JSON format: " + error.message);
+        reject(new Error("Invalid JSON format"));
+      }
+    };
+    fileReader.onerror = error => {
+      reject(new Error(`Error reading file ${file.name}: ${error.message}`));
+    };
+    fileReader.readAsText(file);
+  });
 }
 
 
